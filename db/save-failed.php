e07b8acb8d1f754a801a303f3fcd2174938b670e
junheiLee <jun2ee22@naver.com>
2024-05-30 16:03:55 +0900

    Style: 정렬 정리

--- db/save-failed.php
+++ db/save-failed.php
@@ -3,14 +3,12 @@
 /**
  * PDO로 저장에 실패한 내역
  */
-class SaveFailed
-{
+class SaveFailed {
     
     private $data;
     private $message;
     
-    public function __construct($data, $message)
-    {
+    public function __construct($data, $message) {
         $this->data    = $data;
         $this->message = $message;
     }
